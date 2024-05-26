a8be9f47540324a6f13f17ffc8e86af71fced563
Asura-code <bankaikinowork@gmail.com>
2024-05-26 14:52:21 +0300

    refactor

--- binarysearch/main.go
+++ binarysearch/main.go
@@ -8,21 +8,24 @@ func main() {
 	nums := []int{1,2,3,4,5,6,7}
 	var target int = 4
 	for true{
-		if target != nums[len(nums)-1]{
-		if target < nums[len(nums)/2]{
-			nums = nums[0:len(nums)/2]
-			_ = nums
+		var high int = nums[len(nums)-1]
+		var mid int = len(nums)/2
+		if target != high{
+			if target < nums[mid]{
+				nums = nums[0:mid]
+
+			}else{
+				nums = nums[mid:]
+			}	
+
 		}else{
-			nums = nums[len(nums)/2:]
-			_ = nums
-	}
-	}else{
-		fmt.Println(nums[len(nums)-1],"gg")
-		break
+			fmt.Println(nums[len(nums)-1],"gg")
+			break
 		}
-	if len(nums)==1 && target != nums[len(nums)-1] {
-		fmt.Println("target not in nums")
-		break
-	}
+
+		if len(nums)==1 && target != nums[len(nums)-1] {
+			fmt.Println("target not in nums")
+			break
+		}
 	}
 }
